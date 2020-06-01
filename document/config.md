f997e64fd4ab163513a62e6d0eb5ce1166f24c90
李卓 <lizhuo@guazi.com>
2020-06-01 18:37:32 +0800

    Default Changelist

--- document/config.md
+++ document/config.md
@@ -1,6 +1,9 @@
 docker run -p 7379:6379 --name redis-5mb -v $PWD/redis-config/redis-5mb.conf:/etc/redis/redis.conf redis /etc/redis/redis.conf --appendonly yes  
 docker run -p 7379:6379 --name redis-bloom redislabs/rebloom
+docker run -p 7379:6379 --name redis-normal --appendonly yes  
 
+
+--appendonly yes  
 config set maxmemory-policy allkeys-lru
 
 volatile-lru  
--- src/test/java/com/kiseki/test/TransactionTest.java
+++ src/test/java/com/kiseki/test/TransactionTest.java
@@ -1,9 +1,20 @@
 package com.kiseki.test;
 
 
+import org.junit.Test;
+import org.redisson.api.RBucket;
+import org.redisson.api.RTransaction;
+import org.redisson.api.TransactionOptions;
 
 public class TransactionTest extends BaseTest{
+    @Test
     public void test(){
-        //redissonClient.createTransaction()
+        redissonClient.getBucket("test").set("test");
+
+        RTransaction transaction = redissonClient.createTransaction(TransactionOptions.defaults());
+        RBucket<String> bucket = transaction.getBucket("test");
+        String test = bucket.get();
+        bucket.set(test + "1");
+        transaction.commit();
     }
 }
